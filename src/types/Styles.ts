1ffeb6504a2a3835ea1a0859a60fe5b978594f68
Ravvsky <pawel.rawsk@gmail.com>
2022-11-08 22:28:23 +0100

    feat: add styles interface

---
+++ src/types/Styles.ts
@@ -0,0 +1,14 @@
+export interface stylesInterface {
+  color: {
+    text?: string;
+    background?: string;
+  };
+  typography: {
+    fontSize?: string;
+    fontStyle?: string;
+    fontWeight?: string;
+    letterSpacing?: string;
+    textDecoration?: string;
+    textTransform?: string;
+  };
+}

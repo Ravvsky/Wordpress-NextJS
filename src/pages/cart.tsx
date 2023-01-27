a03390fbec8e994c83451b1eac18566916d7eb62
Ravvsky <pawel.rawsk@gmail.com>
2023-01-27 23:18:32 +0100

    feat: add/edit cart and checkout page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,19 +1,54 @@
+import getCountryByCords from 'api/getCountryByCords';
 import CartItemsList from 'components/Cart/CartItemsList';
 import Container from 'components/Container';
 import CouponForm from 'components/CouponForm';
 import Layout from 'components/Layout';
 import Section from 'components/Section';
 import CartContext from 'context/CartContext';
+import { availableShippingMethods, createCart, updateCustomer } from 'lib/orders';
 import Link from 'next/link';
 import { useContext, useEffect, useState } from 'react';
 
 const CartPage = () => {
   const ctx = useContext(CartContext);
   const [totalAmount, setTotalAmount] = useState(0);
+  const [location, setLocation] = useState('');
+  const [shippingCost, setShippingCost] = useState();
   useEffect(() => {
     setTotalAmount(ctx.totalAmount);
   }, [ctx.totalAmount]);
 
+  useEffect(() => {
+    getCountryByCords()
+      .then((response) => {
+        if (response && response.address) {
+          setLocation(response.address.country_code.toUpperCase());
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    if (location) {
+      const cartProducts: { productId: number; quantity: number }[] = ctx.items.map(({ id, quantity }) => ({
+        productId: id,
+        quantity,
+      }));
+
+      const cartCoupons: string[] = ctx.appliedCoupon.map(({ code }) => code);
+      createCart(cartProducts, cartCoupons).then(() => {
+        updateCustomer(location).then(() =>
+          availableShippingMethods().then((res) => {
+            const shippingMethods = res.cart.cart.availableShippingMethods[0].rates;
+            const cheapestShippingMethod = [...shippingMethods]
+              .sort((a, b) => parseInt(a.cost) - parseInt(b.cost))
+              .find((shippingMethod) => parseInt(shippingMethod.cost) > 0);
+            setShippingCost(cheapestShippingMethod.cost);
+          })
+        );
+      });
+    }
+  }, [location]);
   return (
     <Layout>
       <Section className="bg-[#E8E8E8]">
@@ -25,8 +60,16 @@ const CartPage = () => {
             </div>
             <div className="basis-1/3 bg-white rounded-[2rem] p-[2rem] flex flex-col">
               <h2>Delivery</h2>
-              <div className="flex justify-between text-[2rem]">
-                <span>Total</span> <span>{totalAmount} zł</span>
+              <div className="flex flex-col justify-between text-[2rem]">
+                <div className="flex  justify-between">
+                  <span>Subtotal:</span> <span>{totalAmount} zł</span>
+                </div>
+                <div className="flex  justify-between">
+                  <span>Shipping:</span> <span>{shippingCost} zł</span>
+                </div>
+                <div className="flex  justify-between">
+                  <span>Total:</span> <span>{(+totalAmount + +(shippingCost || 0)).toFixed(2)} zł</span>
+                </div>
               </div>
               <CouponForm />
               <div className="flex flex-col gap-[1rem] mt-auto">
---
+++ src/pages/checkout.tsx
@@ -0,0 +1,143 @@
+import Button from 'components/Button';
+import Container from 'components/Container';
+import Input from 'components/Input';
+import Label from 'components/Label';
+import Layout from 'components/Layout';
+import Section from 'components/Section';
+import CartContext from 'context/CartContext';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { availableShippingMethods, checkoutOrder, createCart } from 'lib/orders';
+import Link from 'next/link';
+import { useContext, useEffect, useState } from 'react';
+import * as Yup from 'yup';
+import { useRouter } from 'next/router';
+import BillingForm from 'components/CheckoutForms/BillingForm/BillingForm';
+import DeliveryForm from 'components/CheckoutForms/DeliveryForm.tsx/DeliveryForm';
+
+const CheckOutPage = () => {
+  const router = useRouter();
+  const ctx = useContext(CartContext);
+
+  const [personalForm, setPersonalForm] = useState();
+  const billingDataHandler = (data) => {
+    setPersonalForm(data);
+  };
+
+  const [deliveryForm, setDeliveryForm] = useState();
+  const deliveryDataHandler = (data) => {
+    if (data) {
+      setDeliveryForm(data);
+    }
+  };
+  const [checkboxValue, setCheckboxValue] = useState(false);
+  const checkboxValueHandler = (data) => {
+    setCheckboxValue(data);
+  };
+
+  const [step, setStep] = useState('');
+  const [shippingMethods, setShippingMethods] = useState([{}]);
+  const [choosenShippingMethod, setChoosenShippingMethod] = useState('');
+  useEffect(() => {
+    setStep(router.query.step || ''), [];
+    const cartProducts: { productId: number; quantity: number }[] = ctx.items.map(({ id, quantity }) => ({
+      productId: id,
+      quantity,
+    }));
+
+    const cartCoupons: string[] = ctx.appliedCoupon.map(({ code }) => code);
+    createCart(cartProducts, cartCoupons).then(() => {
+      availableShippingMethods().then((res) => setShippingMethods(res.cart.cart.availableShippingMethods[0].rates));
+    });
+  }, [ctx.appliedCoupon, ctx.items, router.query.step]);
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+    lastName: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
+  });
+
+  const checkoutOrderHandler = () => {
+    const checkoutOrderData = {
+      customerNote: personalForm.customerNote,
+      billing: {
+        address1: personalForm.address,
+        address2: personalForm.secondAddress,
+        city: personalForm.city,
+        company: personalForm.company,
+        country: personalForm.country,
+        email: personalForm.email,
+        firstName: personalForm.firstName,
+        lastName: personalForm.lastName,
+        overwrite: true,
+        phone: personalForm.phone,
+        postcode: personalForm.postalCode,
+        state: personalForm.state,
+      },
+      shippingMethod: [choosenShippingMethod],
+      shipToDifferentAddress: checkboxValue,
+      shipping: deliveryForm
+        ? {
+            address1: deliveryForm.address,
+            address2: deliveryForm.secondAddress,
+            city: deliveryForm.city,
+            company: deliveryForm.company,
+            country: deliveryForm.country,
+            email: deliveryForm.email,
+            firstName: deliveryForm.firstName,
+            lastName: deliveryForm.lastName,
+            overwrite: true,
+            phone: deliveryForm.phone,
+            postcode: deliveryForm.postalCode,
+            state: deliveryForm.state,
+          }
+        : {},
+    };
+    checkoutOrder(checkoutOrderData);
+  };
+
+  return (
+    <Layout>
+      <Section className="bg-[#ffffff]">
+        <Container>
+          <div className="flex">
+            <div className="flex flex-col p-[2rem]  gap-[2rem]">
+              <section>
+                <h2>{step === 'delivery' ? 'Delivery informations' : 'Personal details'}</h2>
+                <span>If you alread have an account Sign in here</span>
+                {step === '' && <BillingForm personalForm={billingDataHandler} checkboxValue={checkboxValueHandler} />}
+                {step === 'delivery' && checkboxValue && <DeliveryForm deliveryForm={deliveryDataHandler} />}
+                {step === 'delivery' && shippingMethods && (
+                  <div className="flex flex-col">
+                    {shippingMethods.map((shippingMethod, index) => {
+                      return (
+                        <Button
+                          type="button"
+                          key={index}
+                          className="text-black"
+                          onClick={() => {
+                            setChoosenShippingMethod(shippingMethod.id);
+                          }}
+                        >{`${shippingMethod.label} ${shippingMethod.cost} zł`}</Button>
+                      );
+                    })}
+                    <Button type="button" className={undefined} onClick={checkoutOrderHandler}>
+                      CHECKOUT
+                    </Button>
+                  </div>
+                )}
+              </section>
+            </div>
+            <div className="bg-[#383838] w-full px-[2rem]"> s</div>
+          </div>
+        </Container>
+      </Section>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps() {
+  return {
+    props: {},
+  };
+}
+
+export default CheckOutPage;

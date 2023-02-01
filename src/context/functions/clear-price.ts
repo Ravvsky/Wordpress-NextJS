4d7553360b1b353dc2e8830f34a8cc92b95cedac
Ravvsky <pawel.rawsk@gmail.com>
2023-02-01 15:17:07 +0100

    feat: add option to remove separator to clearPrice function

--- src/context/functions/clear-price.ts
+++ src/context/functions/clear-price.ts
@@ -1,6 +1,16 @@
-const clearPrice = ({ price, currency }: { price: string; currency: string }) => {
+const clearPrice = ({
+  price,
+  currency,
+  removeSeparator,
+}: {
+  price: string;
+  currency: string;
+  removeSeparator?: boolean;
+}) => {
   const regex = new RegExp('&nbsp;' + currency, 'g');
-
+  if (removeSeparator) {
+    return Number(price.replace(regex, '').replace(/,/g, ''));
+  }
   return Number(price.replace(regex, '').replace(/,/g, '.'));
 };
 
